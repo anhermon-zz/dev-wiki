f322e9f898d74f4b2b821be83556b2103aec648a
anhermon <angel_mail@gmx.com>
2015-06-13 23:26:09 +0300

    add new feature

--- dev-wiki-api/src/main/java/org/angel/devWiki/api/App.java
+++ dev-wiki-api/src/main/java/org/angel/devWiki/api/App.java
@@ -2,15 +2,12 @@ package org.angel.devWiki.api;
 
 import java.io.UnsupportedEncodingException;
 
-import org.angel.devWiki.api.filters.JWTAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 @SpringBootApplication
 @Qualifier("dev-wiki.api.config")
@@ -23,10 +20,13 @@ public class App {
 	public static void main(String[] args) throws UnsupportedEncodingException {
 		SpringApplication.run(App.class, args);
 	}
-	
-	@Bean
-	public OncePerRequestFilter validationFilter(){
-		return new JWTAuthFilter();
+	public String newFeature(){
+		return "new Feature";
 	}
+	
+//	@Bean
+//	public OncePerRequestFilter validationFilter(){
+//		return new JWTAuthFilter();
+//	}
 
 }
