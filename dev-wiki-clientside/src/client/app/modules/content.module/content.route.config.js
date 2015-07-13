9dcc40addd09a019e763e89182fec73e74c09037
anhermon <angel_mail@gmx.com>
2015-07-13 15:51:48 +0300

    materiale deisgn style

--- dev-wiki-clientside/src/client/app/modules/content.module/content.route.config.js
+++ dev-wiki-clientside/src/client/app/modules/content.module/content.route.config.js
@@ -7,9 +7,23 @@
     function stateProviderConfig ($stateProvider) {
         $stateProvider
             .state('home.articles', {
+                abstract: true,
                 url: '/articles',
                 views: {
-                    'center@':  {template: '<data-ang-content-main></data-ang-content-main>'}
+                    'center@':  {template: '<data-ang-content-articles></data-ang-content-articles>'},
+                    'article.body@': {template: 'test'}
+                }
+            })
+            .state('home.articles.read', {
+                url: '/read',
+                views: {
+                    'test': {template: 'read'}
+                }
+            })
+            .state('home.articles.write', {
+                url: '/write',
+                views: {
+                    'article.body@home.articles.write': {template: 'write'}
                 }
             });
     }
--- dev-wiki-clientside/src/client/app/modules/content.module/directives/content.main.directive.js
+++ dev-wiki-clientside/src/client/app/modules/content.module/directives/content.main.directive.js
@@ -1,14 +1,14 @@
 (function(angular) {
     'use strict';
     angular.module('ang.content')
-    .directive('angContentMain', angContentMain);
+    .directive('angContentArticles', angContentMain);
     ///////////////////
     function angContentMain() {
         return {
             replace: false,
             controller :   'angContentMainCtrl',
             controllerAs : 'amcc',
-            templateUrl :  'app/modules/content.module/templates/main.tpl.html',
+            templateUrl :  'app/modules/content.module/templates/articles.tpl.html',
             link: function(scope, elem, attrs) {
                 var vm = scope.amcc;
                 console.log('flag');
